6571025ee69a12c50edb29685df7ac62d727de4c
dplubons <42813876+dplubons@users.noreply.github.com>
2019-02-04 18:49:28 -0500

    Update database.php

--- database.php
+++ database.php
@@ -3,10 +3,10 @@ class Database
 {
 	// these are not the real passwords
 	// for real passwords, see file in ../database subdirectory
-	private static $dbName = '10.8.30.49' ; 
-	private static $dbHost = 'dplubons355wi19' ;
+	private static $dbName = 'dplubons355wi19' ; 
+	private static $dbHost = '10.8.30.49' ;
 	private static $dbUsername = 'dplubons355wi19';
-	private static $dbUserPassword = 'root';
+	private static $dbUserPassword = '********';
 	
 	private static $cont  = null;
 	
